c40462e90c621350e735fec07b3086c33c284ae9
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-11 16:54:40 +0100

    add: postspage component

---
+++ src/components/PostsPage.jsx
@@ -0,0 +1,15 @@
+import PostList from "../pages/PostList";
+
+export default function PostsPage(){
+
+    return (
+      <>
+            <div className="container">
+                <h1>the post</h1>
+            </div>
+            <div className="container">
+                <PostList />
+            </div>
+      </>  
+    )
+}
